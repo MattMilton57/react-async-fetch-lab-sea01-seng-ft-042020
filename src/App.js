ceb969739efcca897c4ce251322d5f0f1c128133
github username <github email address>
2020-08-10 16:47:58 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
-// create your App component here
+import React from 'react';
+
+class App extends React.Component {
+
+    componentDidMount(){
+        fetch ("http://api.open-notify.org/astros.json")
+        .then (json => json.stringify)
+    }
+
+  render(){
+    return(<li>a thing</li>)
+  }
+
+} export default App
